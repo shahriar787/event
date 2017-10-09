ef5a9e30c876fe593cfef9dfc4346a409372f843
Shahriar Shakil <30352466+shahriar787@users.noreply.github.com>
2017-10-10 03:33:24 +0600

    Add files via upload

---
+++ js/event.js
@@ -0,0 +1,15 @@
+
+
+function onClk(){
+	alert("Hello world");
+};
+
+function selectFunc(){
+	
+	alert("You select some text");
+	
+}
+
+function changeEvent(){
+	alert("You are changing event now..");	
+}
